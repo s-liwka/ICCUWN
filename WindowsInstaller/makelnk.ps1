c282bd20d5739e7a6c1b4334bb681ce8878dd939
sliwka <116428267+s-liwka@users.noreply.github.com>
2023-08-22 19:09:32 +0200

    aaaa

--- WindowsInstaller/makelnk.ps1
+++ WindowsInstaller/makelnk.ps1
@@ -1,3 +1,6 @@
 $source = "$env:LOCALAPPDATA\ICCUWN\run.bat"
-$destination = "$env:USERPROFILE\Desktop\ICCUWN"
-New-Item -ItemType SymbolicLink -Path $destination -Target $source
+$destination = "$env:USERPROFILE\Desktop\ICCUWN.lnk"
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut($destination)
+$shortcut.TargetPath = $source
+$shortcut.Save()
