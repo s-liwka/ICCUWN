04fa5c75338319c042bf7a4512c0bd77916ada77
sliwka <116428267+s-liwka@users.noreply.github.com>
2023-08-22 19:25:37 +0200

    Delete makelnk.ps1

--- WindowsInstaller/makelnk.ps1
+++
@@ -1,6 +0,0 @@
-$source = "$env:LOCALAPPDATA\ICCUWN\run.bat"
-$destination = "$env:USERPROFILE\Desktop\ICCUWN.lnk"
-$shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut($destination)
-$shortcut.TargetPath = $source
-$shortcut.Save()
