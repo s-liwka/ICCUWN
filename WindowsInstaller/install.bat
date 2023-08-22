9660953104648403b66e68caf2f491a0d7994966
sliwka <116428267+s-liwka@users.noreply.github.com>
2023-08-22 19:26:11 +0200

    Add files via upload

---
+++ WindowsInstaller/install.bat
@@ -0,0 +1,15 @@
+@echo off
+mkdir %localappdata%\ICCUWN
+echo Downloading iccuwn.py and run.bat...
+curl -o %localappdata%\ICCUWN\iccuwn.py https://raw.githubusercontent.com/s-liwka/ICCUWN/main/iccuwn.py
+curl -o %localappdata%\ICCUWN\run.bat https://raw.githubusercontent.com/s-liwka/ICCUWN/main/WindowsInstaller/run.bat
+echo Creating venv
+python -m venv %localappdata%\ICCUWN\iccuwn-venv
+call %localappdata%\ICCUWN\iccuwn-venv\Scripts\activate.bat
+echo Installing requirements
+curl -o %localappdata%\ICCUWN\requirements.txt https://raw.githubusercontent.com/s-liwka/ICCUWN/main/requirements.txt
+pip install -r requirements.txt
+set "source=%localappdata%\ICCUWN\run.bat"
+set "destination=%userprofile%\Desktop\ICCUWN.lnk"
+powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%destination%'); $Shortcut.TargetPath = '%source%'; $Shortcut.Save()"
+%localappdata%\ICCUWN\run.bat
